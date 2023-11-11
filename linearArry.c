bbd515df4583f6272152f9cb38bb38ddb9be9c19
Dhanush <dhanushpreji01@gmail.com>
2023-11-11 11:28:35 +0530

    added Deletion

--- linearArry.c
+++ linearArry.c
@@ -1,9 +1,14 @@
-//consist of Traversing , Adding and Deleting of an element in an array
-
 
 #include <stdio.h>
-#include <stdlib.h>
-
+ #include <stdlib.h>
+void display(int arr[],int size)
+{
+    for (int i = 0; i <= size; i++)
+    {
+        printf("Array[%d] = %d\n",i,arr[i]);
+    }
+    
+}
 void add(int  arr[], int element, int index,int newsize)
 {
 
@@ -17,14 +22,27 @@ void add(int  arr[], int element, int index,int newsize)
     {
         printf("arry %d index[%d]\n",arr[j],j);
     }
+ }
+int delete(int arr[],int index,int size)
+{
+    for(int i = index;i<size - 1;i++)
+    {
+        arr[i] = arr[i+1];
+    }
 }
-
 
 int main()
 {
-    int ptr[] = {1,2,3,4,5,7};
+    int arr[] = {1,2,3,5,6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int index = 2;
+ 
+    // Delete x from arr[]
+    delete(arr,index,size);
+    size-=2;
+    display(arr,size);
     
-    int newsize = sizeof(ptr)/sizeof(ptr[0]);
-    add(ptr,6,5,newsize);
+
+    
 
 }
