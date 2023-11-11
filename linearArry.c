57c4b150a5f2e8b248ca83c7c9efced1e9f138f7
Dhanush <dhanushpreji01@gmail.com>
2023-11-11 11:42:12 +0530

    Deletion and insertion

--- linearArry.c
+++ linearArry.c
@@ -3,25 +3,25 @@
  #include <stdlib.h>
 void display(int arr[],int size)
 {
-    for (int i = 0; i <= size; i++)
+    for (int i = 0; i <= size-1; i++)
     {
         printf("Array[%d] = %d\n",i,arr[i]);
     }
     
 }
-void add(int  arr[], int element, int index,int newsize)
+int addElement(int  arr[], int element, int index,int capacity,int size)
 {
 
-    newsize++;
-    for(int i = 0;i<= newsize;i++)
+    if(size >= capacity)
+    {
+        return - 1;
+    }
+    for(int i = size -1;i>=index;i--)
     {
         arr[index+1] = arr[index];
     }
     arr[index] = element;
-    for(int j = 0; j <=newsize-1;j++)
-    {
-        printf("arry %d index[%d]\n",arr[j],j);
-    }
+    return 1;
  }
 int delete(int arr[],int index,int size)
 {
@@ -33,13 +33,16 @@ int delete(int arr[],int index,int size)
 
 int main()
 {
-    int arr[] = {1,2,3,5,6};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int arr[10] = {1,2,3,5,6};
+    int size = 5;
     int index = 2;
- 
-    // Delete x from arr[]
+    addElement(arr,7,5,10,size);
+    size+=1;
+    display(arr,size);
+    // // Delete x from arr[]
+    printf("Deletion\n");
     delete(arr,index,size);
-    size-=2;
+    size-=1;
     display(arr,size);
     
 
