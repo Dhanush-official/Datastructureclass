8bd08605b1dafc1c0b65ab85dc536bc4908b3ccd
Dhanush <dhanushpreji01@gmail.com>
2023-11-12 19:39:58 +0530

    insetion

--- linked_list_tra_ins_del.c
+++ linked_list_tra_ins_del.c
@@ -19,7 +19,64 @@ void LinkedListTraversal(struct Node  *ptr)
         ptr = ptr->next;
     }
 }
+void SearchValue(struct Node * ptr,int data)
+{
+    int i = 0;
+    while(ptr->data != data)
+    {
+        ptr = ptr->next;
+        i++;
+    }
+    printf("Element[%d] Found in Node at index[%d]",ptr->data,i);
+}
+struct Node * insertAtfirst(struct Node * head, int data)
+{
+    struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->next = head;
+    ptr->data = data;
+    return ptr;
+}
+struct Node * insertAtIndex(struct Node *head,int data,int index)
+{
+    struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node * p = head;
+    int i = 0;
 
+    while(i != index-1)
+    {
+        p = p->next;
+        i++;
+    }
+    ptr->data = data; 
+    ptr->next = p->next;
+    p->next   = ptr;
+    return head;
+
+}
+struct Node * insertAtEnd(struct Node * head,int data)
+{
+        struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+        ptr->data = data;
+        struct Node * p = head;
+        while(p->next != NULL)
+        {
+            p = p->next;
+        }
+        p->next = ptr;
+        ptr->next = NULL;
+
+        return head;
+}
+struct Node * insertAtNode(struct Node * head, struct Node *prevNode,int data)
+{
+    struct Node * ptr = (struct Node * )malloc(sizeof(struct Node));
+    ptr->data =data;
+
+    ptr->next = prevNode->next;
+    prevNode->next = ptr;
+
+    return head;
+}
 int main()
 {
     struct Node *head;
@@ -44,6 +101,14 @@ int main()
     fourth->data = 40;
     fourth->next = NULL;
 
+    LinkedListTraversal(head);
+    //If you wanted to use them just uncomment t
+    // head = insertAtfirst(head,15);
+    // head = insertAtIndex(head,56,3);
+    // head = insertAtEnd(head,33);
+    // SearchValue(head,20);
+    // insertAtNode(head,second,35);
+    printf("\n\n");
     LinkedListTraversal(head);
 
 }
